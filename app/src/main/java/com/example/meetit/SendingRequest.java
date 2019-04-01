5465636115d89a7c7822be77e049765ea5ed436b
pimmeijer <pim_meijer@outlook..com>
2019-04-01 13:09:00 +0200

    Write meetingrequest to the database

--- app/src/main/java/com/example/meetit/SendingRequest.java
+++ app/src/main/java/com/example/meetit/SendingRequest.java
@@ -1,13 +1,112 @@
 package com.example.meetit;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class SendingRequest extends AppCompatActivity {
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mMeetingRequestDatabaseReference;
+
+    Button btnDatePicker, btnTimePicker, btnSubmit;
+    EditText txtDate, txtTime, txtTitle, txtLocation, txtNotes;
+    private int mYear, mMonth, mDay, mHour, mMinute;
+    private String dateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sendingrequest);
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mMeetingRequestDatabaseReference = mFirebaseDatabase.getReference().child("meetingrequests");
+
+        txtTitle = (EditText) findViewById(R.id.title);
+
+        btnDatePicker=(Button)findViewById(R.id.btn_date);
+        btnTimePicker=(Button)findViewById(R.id.btn_time);
+        txtDate=(EditText)findViewById(R.id.in_date);
+        txtTime=(EditText)findViewById(R.id.in_time);
+        txtLocation=(EditText)findViewById(R.id.location);
+        txtNotes=(EditText)findViewById(R.id.notes);
+        btnSubmit = (Button) findViewById(R.id.submit);
+
+        btnDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Get Current Date
+                final Calendar c = Calendar.getInstance();
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(SendingRequest.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+
+                                txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
+
+        btnTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get Current Time
+                final Calendar c = Calendar.getInstance();
+                mHour = c.get(Calendar.HOUR_OF_DAY);
+                mMinute = c.get(Calendar.MINUTE);
+
+                // Launch Time Picker Dialog
+                TimePickerDialog timePickerDialog = new TimePickerDialog(SendingRequest.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay,
+                                                  int minute) {
+
+                                txtTime.setText(hourOfDay + ":" + minute);
+                            }
+                        }, mHour, mMinute, false);
+                timePickerDialog.show();
+            }
+        });
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dateTime = txtDate.getText().toString() + " " + txtTime.getText().toString();
+                MeetingRequest request = new MeetingRequest(txtTitle.getText().toString(), dateTime, txtLocation.getText().toString(), txtNotes.getText().toString());
+                mMeetingRequestDatabaseReference.push().setValue(request);
+                txtTime.setText("");
+                txtDate.setText("");
+                txtLocation.setText("");
+                txtNotes.setText("");
+                txtTitle.setText("");
+
+                Toast.makeText(SendingRequest.this, "Meeting request sent!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
