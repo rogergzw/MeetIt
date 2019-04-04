5f6d30060b13de88325539d1e21490c7eef3bf1b
rogergh <r.g.z.w.goh@student.tue.nl>
2019-04-04 12:46:08 +0200

    3 Apr Update

--- app/src/main/java/com/example/meetit/UserProfile.java
+++ app/src/main/java/com/example/meetit/UserProfile.java
@@ -1,11 +1,13 @@
 package com.example.meetit;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,14 +15,20 @@ import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseUser;
 
 public class UserProfile extends AppCompatActivity {
+    private TextView textView;
+    private EditText editText;
+    private Button saveButton;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String TEXT = "text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
-        Button signoutButton = findViewById(R.id.signout);
-        TextView name = findViewById(R.id.name);
+        Button signoutButton = findViewById(R.id.signout_button);
+        EditText name = findViewById(R.id.name);
         name.setText(MainActivity.mUsername);
 
         signoutButton.setOnClickListener(new View.OnClickListener() {
